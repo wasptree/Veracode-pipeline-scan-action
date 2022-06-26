6870de2922ab5d9f46e4c73f42a35176292c5d76
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2022-06-26 12:17:14 +0200

    commit

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,18 @@ import { downloadJar } from "./pipeline-scan";
 import { runScan } from "./pipeline-scan";
 
 
-// get input params
-const parameters = []
-const vid = core.getInput('vid', {required: true} );
-parameters.push(vid)
-const vkey = core.getInput('vkey', {required: true} );
-parameters.push(vkey)
-const file = core.getInput('file', {required: true} );
-parameters.push(file)
-
 function run (){
+    // get input params
+    const parameters = []
+    const vid = core.getInput('vid', {required: true} );
+    parameters.push(vid)
+    const vkey = core.getInput('vkey', {required: true} );
+    parameters.push(vkey)
+    const file = core.getInput('file', {required: true} );
+    parameters.push(file)
+
+    console.log(parameters)
+
     downloadJar()
 
     runScan(parameters)
