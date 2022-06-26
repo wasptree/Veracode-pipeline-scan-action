c63cad957a100f2f151c5084a54d9a594a89ac93
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2022-06-26 17:03:33 +0200

    store scan command output

--- src/pipeline-scan.ts
+++ src/pipeline-scan.ts
@@ -39,11 +39,12 @@ export function runScan (parameters){
     var spawn = require('child_process').spawn;
     var getScanCommandOutput = spawn('sh',['-c', scanCommand],{stdio:"pipe",});
     //spit stdout to screen
+    core.info('1. output')
     core.info( getScanCommandOutput.stdout.on('data', function (data) {   process.stdout.write(data.toString());  }) )
     //spit stderr to screen
+    core.info('2. output')
     core.info( getScanCommandOutput.stderr.on('data', function (data) {   process.stdout.write(data.toString());  }) )
 
     getScanCommandOutput.on('close', function (code) { 
         core.info("Finished with code " + code);
-    core.info(getScanCommandOutput)
 }
