444c6bb00214070df77aeb17eb4a468235bd930d
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2022-07-09 12:14:23 +0200

    create scna command

--- dist/check-parameters.js
+++ dist/check-parameters.js
@@ -24,7 +24,13 @@ const core = __importStar(require("@actions/core"));
 function checkParameters(parameters) {
     core.info(JSON.stringify(parameters));
     if (parameters.run_method == "runScan") {
-        core.info('run stage');
+        core.info('simple run stage');
+        let command;
+        Object.entries(parameters).forEach(([key, value], index) => {
+            core.info(key, value, index);
+            command += "--" + key + " " + value;
+            core.info(command);
+        });
     }
     else if (parameters.run_method == "storeBaseline") {
         core.info('baseline file stage');
--- src/check-parameters.ts
+++ src/check-parameters.ts
@@ -5,7 +5,14 @@ export function checkParameters (parameters)  {
     core.info(JSON.stringify(parameters))
 
     if ( parameters.run_method == "runScan" ){
-        core.info('run stage')
+        core.info('simple run stage')
+        let command:string
+
+        Object.entries(parameters).forEach(([key, value], index) => {
+            core.info(key, value, index);
+            command += "--"+key+" "+value
+            core.info(command)
+        });
 
     }
     else if ( parameters.run_method == "storeBaseline" ) {
