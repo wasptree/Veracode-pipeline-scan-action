3ca4f505e77492f6edc7f135544101c79b946617
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2022-07-09 12:27:54 +0200

    work with params

--- src/check-parameters.ts
+++ src/check-parameters.ts
@@ -7,10 +7,17 @@ export function checkParameters (parameters)  {
     if ( parameters.run_method == "runScan" ){
         core.info('simple run stage')
         let command:string = ""
+        
+        if ( parameters.request_policy != ""){
+            core.info('Policy file download required')
+            command = 'java -jar -vid '+parameters.vid+' -vkey '+vkey+' --request_policy "'+parameters.request_policy+'"'
+            core.info('Policy Download command: '+command)
+        }
+        
 
         Object.entries(parameters).forEach(([key, value], index) => {
             core.info(key, value, index);
-            if ( key != 'vid' || key != 'vkey' || key != 'run_method') {
+            if ( key != 'vid' && key != 'vkey' && key != 'run_method' && key != 'request_policy') {
                 command += " --"+key+" "+value  
                 core.info(command)
             }
